193570641b2c3b32e7972fff9c4c47ee8ec8067c
Noah DeCambra <70386429+noahdecambra@users.noreply.github.com>
2022-10-23 02:22:05 -0400

    Add files via upload

---
+++ AudioManager.cs
@@ -0,0 +1,74 @@
+using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+	public static AudioManager instance;
+
+	public Sound[] sounds;
+
+	void Awake()
+	{
+		if (instance != null)
+		{
+			Destroy(gameObject);
+		}
+		else
+		{
+			instance = this;
+			DontDestroyOnLoad(gameObject);
+		}
+
+		foreach (Sound s in sounds)
+		{
+			s.source = gameObject.AddComponent<AudioSource>();
+			s.source.clip = s.clip;
+			s.source.loop = s.loop;
+			s.source.volume = s.volume;
+		}
+	}
+
+	void Start()
+	{
+		Play("MainMusic");
+	}
+
+	public void Play(string name)
+	{
+		Sound s = Array.Find(sounds, sound => sound.name == name);
+		if (s == null)
+		{
+			Debug.LogWarning("Sound: " + name + " not found!");
+			return;
+		}
+		s.source.Play();
+	}
+
+	public void StopPlaying(string sound)
+	{
+		Sound s = Array.Find(sounds, item => item.name == sound);
+		
+		if(s == null)
+		{
+			Debug.LogWarning("Sound:" + name + "not found!");
+			return;
+		}
+
+		s.source.Stop();
+	}
+
+	public void Pause(string sound)
+	{
+		Sound s = Array.Find(sounds, item => item.name == sound);
+		
+		if(s == null)
+		{
+			Debug.LogWarning("Sound:" + name + "not found!");
+			return;
+		}
+
+		s.source.Pause();
+	}
+}
